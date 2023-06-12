03b02b022870d424042dc757b4b4e6d54d665055
kenyi21 <kenyi6al@gmail.com>
2023-06-12 11:42:56 -0500

    avance 3

--- ec1-J19/src/main/java/pe/edu/idat/ec1J19/DiasRepository.java
+++ ec1-J19/src/main/java/pe/edu/idat/ec1J19/DiasRepository.java
@@ -21,36 +21,43 @@ public class DiasRepository {
 		Dia lunes = new Dia();
 		lunes.setName("lunes");
 		lunes.setTraduccion("monday");	
+		lunes.setcolor("amarillo");
 		dias.put(lunes.getName(), lunes);
 
         Dia martes = new Dia();
 		martes.setName("martes");
 		martes.setTraduccion("Tuesday");	
+		martes.setcolor("verde");
 		dias.put(martes.getName(),martes);
 
         Dia miercoles = new Dia();
 		miercoles.setName("miercoles");
 		miercoles.setTraduccion("Wednesday");	
+		miercoles.setcolor("azul");
 		dias.put(miercoles.getName(), miercoles);
 
         Dia jueves = new Dia();
 		jueves.setName("jueves");
 		jueves.setTraduccion("Thursday");	
+		jueves.setcolor("rojo");
 		dias.put(jueves.getName(), jueves);
 
         Dia viernes = new Dia();
 		viernes.setName("viernes");
-		viernes.setTraduccion("Friday");	
+		viernes.setTraduccion("Friday");
+		viernes.setcolor("naranja");	
 		dias.put(viernes.getName(), viernes);
 
         Dia sabado = new Dia();
 		sabado.setName("sabado");
-		sabado.setTraduccion("Saturday");	
+		sabado.setTraduccion("Saturday");
+		sabado.setcolor("negro");	
 		dias.put(sabado.getName(), sabado);
 
         Dia domingo = new Dia();
 		domingo .setName("domingo");
 		domingo .setTraduccion("Sunday");	
+		domingo.setcolor("blanco");
 		dias.put(domingo .getName(),domingo );
 	
 	}
