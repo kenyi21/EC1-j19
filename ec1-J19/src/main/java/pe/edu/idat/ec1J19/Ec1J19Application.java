f06ddcd0e009e4aabc850ecd1666651e8cbc317b
kenyi21 <kenyi6al@gmail.com>
2023-06-12 10:18:08 -0500

    avance

---
+++ ec1-J19/src/main/java/pe/edu/idat/ec1J19/Ec1J19Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec1J19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec1J19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec1J19Application.class, args);
+	}
+
+}
---
+++ ec1-J19/src/test/java/pe/edu/idat/ec1J19/Ec1J19ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec1J19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec1J19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
